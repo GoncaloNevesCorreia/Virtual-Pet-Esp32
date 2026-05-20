6c1bb1de05f7858073d722740f3c7534b06de7b9
Gonçalo Correia <goncalojose.nevescorreia@gmail.com>
2026-05-20 15:15:23 +0100

    WIP Energy - Criação do namespace de energia

---
+++ src/Game/Energy/Energy.cpp
@@ -0,0 +1,12 @@
+#include "Energy.h"
+
+namespace Pet_Energy {
+
+// Timer healthDecreaseTimer(decrease, BASE_DECAY_SPEED);
+
+void init() {
+}
+
+void render() {
+}
+}  // namespace Pet_Energy
---
+++ src/Game/Energy/Energy.h
@@ -0,0 +1,20 @@
+#ifndef PET_ENERGY_H
+#define PET_ENERGY_H
+
+#include <Animate.h>
+#include <Timer.h>
+
+#include "Game/Game.h"
+
+namespace Pet_Energy {
+
+void init();
+
+void render();
+
+void increase();
+
+void decrease();
+}  // namespace Pet_Energy
+
+#endif
